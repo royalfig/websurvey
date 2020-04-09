87cc539900c232d580ba3a9a9112b5610104efb9
Ryan Feigenbaum <rfeigenb@ND.EDU>
2020-04-09 14:41:17 -0400

    moved dependencies

--- gridsome.server.js
+++ gridsome.server.js
@@ -6,15 +6,15 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 const axios = require("axios");
 require("dotenv").config();
-const path = require("path");
-console.log(path.join(__dirname, "/utils/MatrixGraph"));
+// const path = require("path");
+// console.log(path.join(__dirname, "/utils/MatrixGraph"));
 
-const MatrixGraph = require(path.join(__dirname, "static/utils/MatrixGraph"));
-const PieGraph = require(path.join(__dirname, "static/utils/PieGraph"));
-const CheckBoxData = require(path.join(__dirname, "static/utils/CheckboxData"));
-const Country = require(path.join(__dirname, "static/utils/Country"));
-const StandardPie = require(path.join(__dirname, "static/utils/StandardPie"));
-const Comments = require(path.join(__dirname, "static/utils/Comments"));
+// const MatrixGraph = require(path.join(__dirname, "static/utils/MatrixGraph"));
+// const PieGraph = require(path.join(__dirname, "static/utils/PieGraph"));
+// const CheckBoxData = require(path.join(__dirname, "static/utils/CheckboxData"));
+// const Country = require(path.join(__dirname, "static/utils/Country"));
+// const StandardPie = require(path.join(__dirname, "static/utils/StandardPie"));
+// const Comments = require(path.join(__dirname, "static/utils/Comments"));
 
 module.exports = function(api) {
   const websurveyID =
@@ -179,3 +179,454 @@ const allValues = (data, property, type) => {
   if (type === radio) {
   }
 };
+
+class CheckBoxData {
+  constructor(data, filter, name, type) {
+    this.name = name;
+    this.filteredData = data.filter(item => item.data[filter]);
+    this.totalItems = this.filteredData.map(item => item.data[filter].value);
+    this.splitData = this.totalItems.map(item => item.split(/\n/));
+    this.allItems = this.splitData.flat();
+    this.uniqueList = [...new Set(this.allItems)].sort();
+    this.aggregatedData = this.countData();
+    this.configObjectPie = {
+      series: this.aggregatedData,
+      chartOptions: {
+        chart: {
+          type: "pie",
+          fontFamily:
+            "Roboto,-apple-system, system-ui, BlinkMacSystemFont, 'Segoe UI', Roboto,'Helvetica Neue', Arial, sans-serif"
+        },
+        labels: this.uniqueList,
+        title: {
+          text: this.name,
+          align: "center"
+        },
+        legend: {
+          position: "bottom",
+          horizontalAlign: "center"
+        }
+      }
+    };
+    this.configObjectBar = {
+      series: [{ data: this.aggregatedData }],
+      chartOptions: {
+        chart: {
+          type: "bar",
+          fontFamily:
+            "Roboto,-apple-system, system-ui, BlinkMacSystemFont, 'Segoe UI', Roboto,'Helvetica Neue', Arial, sans-serif"
+        },
+        title: {
+          text: this.name,
+          align: "center"
+        },
+        legend: {
+          position: "bottom",
+          horizontalAlign: "center"
+        },
+        plotOptions: {
+          bar: {
+            horizontal: true
+          }
+        },
+        xaxis: {
+          categories: this.uniqueList
+        }
+      }
+    };
+    this.configObject =
+      type === "pie" ? this.configObjectPie : this.configObjectBar;
+  }
+  // Methods
+  countData() {
+    const countedData = this.uniqueList.map(item => {
+      let count = 0;
+      for (let i = 0; i < this.allItems.length; i++) {
+        if (this.allItems[i] === item) {
+          count++;
+        }
+      }
+      return count;
+    });
+    return countedData;
+  }
+}
+
+class Comments {
+  constructor(data, filter, name) {
+    this.name = name;
+    this.filteredData = data.filter(item => item.data[filter]);
+    this.totalItems = this.filteredData.map(item => item.data[filter].value);
+  }
+}
+
+const emojiFlags = require("emoji-flags");
+
+class Country {
+  constructor(data, filter, name) {
+    this.name = name;
+    this.filteredData = data.filter(item => item.data[filter]);
+    this.totalItems = this.filteredData.map(item => item.data[filter].value);
+    this.countriesWithoutOther = this.totalItems.map(country =>
+      country.replace("Other: ", "").replace(/USA?/i, "United States")
+    );
+    this.preferNotObj = {
+      name: "Prefer not to answer",
+      emoji: "⭕",
+      title: null,
+      unicode: null,
+      title: null,
+      code: null
+    };
+
+    this.countryEmojis = this.countriesWithoutOther.map(country => {
+      if (country !== "Prefer not to answer") {
+        const index = emojiFlags.names.findIndex(
+          element => element === country
+        );
+        return emojiFlags.data[index];
+      }
+
+      return this.preferNotObj;
+    });
+
+    this.uniqueEmojis = [...new Set(this.countryEmojis)];
+
+    this.countriesCounted = this.uniqueEmojis.map(item => {
+      item.count = 0;
+      this.countryEmojis.map(item2 => {
+        if (item2 === item) {
+          item.count++;
+        }
+      });
+      return item;
+    });
+
+    this.countriesCountedAndSorted = this.countriesCounted.sort((a, b) =>
+      a.name > b.name ? 1 : -1
+    );
+    this.preferNotIndex = this.countriesCountedAndSorted.findIndex(
+      element => element.name === "Prefer not to answer"
+    );
+    this.reordered = this.reorderedArray();
+    this.data = this.countriesCountedAndSorted.map(country => country.count);
+    this.categories = this.countriesCountedAndSorted.map(
+      country => country.name + " " + country.emoji
+    );
+    this.nameAndData = this.countriesCountedAndSorted.map(
+      country => country.name + ": " + country.count
+    );
+    this.flags = this.countriesCountedAndSorted.map(country => {
+      if (country.name === "Prefer not to answer") {
+        return `<svg height="100%" width="100%" viewBox="0 0 24 24">
+        <path fill="currentColor" d="M20.84 22.73L19.1 21C19.06 21 19.03 21 19 21H5C3.9 21 3 20.11 3 19V5C3 4.97 3 4.94 3 4.9L1.11 3L2.39 1.73L22.11 21.46L20.84 22.73M21 5C21 3.89 20.1 3 19 3H6.2L21 17.8V5Z" />
+    </svg>`;
+      }
+      return `<img src="https://www.countryflags.io/${country.code}/flat/64.png">`;
+    });
+    this.flagCount = {
+      flag: this.flags,
+      data: this.data,
+      names: this.nameAndData
+    };
+    this.configObject = {
+      series: [{ data: this.data }],
+      chartOptions: {
+        chart: {
+          type: "bar",
+          fontFamily:
+            "Roboto,-apple-system, system-ui, BlinkMacSystemFont, 'Segoe UI', Roboto,'Helvetica Neue', Arial, sans-serif"
+        },
+        title: {
+          text: this.name,
+          align: "center"
+        },
+        legend: {
+          position: "bottom",
+          horizontalAlign: "center"
+        },
+        plotOptions: {
+          bar: {
+            horizontal: true
+          }
+        },
+        xaxis: {
+          categories: this.categories
+        }
+      }
+    };
+  }
+
+  reorderedArray() {
+    const preferNot = this.countriesCountedAndSorted.splice(
+      this.preferNotIndex,
+      1
+    );
+    this.countriesCountedAndSorted.push(preferNot[0]);
+  }
+}
+
+class MatrixGraph {
+  constructor(data, filter, name) {
+    this.name = name;
+    this.filteredData = data.filter(item => item.data[filter]);
+    this.totalItems = this.filteredData.map(item => item.data[filter].value);
+    this.convertedToArray = this.totalItems.join("\n").split(/\n/);
+    this.uniqueKeys = [
+      ...new Set(this.convertedToArray.map(key => this.getKey(key)))
+    ];
+    this.uniqueKeysSorted = this.uniqueKeys.sort();
+    this.uniqueProps = [
+      ...new Set(this.convertedToArray.map(prop => this.getProp(prop)))
+    ];
+    this.uniquePropsSorted = this.sortProps();
+    this.keyArray = this.uniqueKeysSorted.map(x => {
+      const obj = { type: x };
+      return obj;
+    });
+    this.propArray = this.uniquePropsSorted.map(x => {
+      const obj = { [x]: 0 };
+      return obj;
+    });
+    this.series = this.getSeries();
+    this.configObject = {
+      series: this.series,
+      chartOptions: this.getConfig()
+    };
+  }
+
+  //Methods
+  getProp(prop) {
+    return prop.match(/[/A-Za-z ]+$/)[0].trim();
+  }
+  getKey(key) {
+    return key.match(/([A-Za-z& ]+)/)[0].trim();
+  }
+
+  sortProps() {
+    if (this.uniqueProps.includes("Very Useful")) {
+      return ["Very Useful", "Somewhat Useful", "Not Useful", "N/A"];
+    }
+
+    if (this.uniqueProps.includes("Strongly Agree")) {
+      return [
+        "Strongly Agree",
+        "Agree",
+        "Neutral",
+        "Disagree",
+        "Strongly Disagree"
+      ];
+    }
+
+    if (this.uniqueProps.includes("Frequently")) {
+      return ["Frequently", "Sometimes", "Never"];
+    }
+
+    return this.uniqueProps.sort();
+  }
+
+  getSeries() {
+    // Modify in place
+    this.keyArray.map(x => {
+      this.propArray.map(y => {
+        Object.assign(x, y);
+      });
+    });
+
+    this.convertedToArray.forEach(x => {
+      const key = this.getKey(x);
+      const prop = this.getProp(x);
+      this.keyArray.forEach(y => {
+        if (y.type === key) {
+          y[prop]++;
+        }
+      });
+    });
+
+    const series = this.uniquePropsSorted.map(x => {
+      const name = x;
+      const data = [];
+      this.keyArray.forEach(y => {
+        data.push(y[x]);
+      });
+      return { name: name, data: data };
+    });
+
+    return series;
+  }
+
+  getConfig() {
+    const chartOptions = {
+      chart: {
+        type: "bar",
+        height: 350,
+        stacked: true,
+        stackType: "normal",
+        fontFamily:
+          "Roboto,-apple-system, system-ui, BlinkMacSystemFont, 'Segoe UI', Roboto,'Helvetica Neue', Arial, sans-serif"
+      },
+      plotOptions: {
+        bar: {
+          horizontal: true
+        }
+      },
+      stroke: {
+        width: 1,
+        colors: ["#fff"]
+      },
+      title: {
+        text: this.name,
+        align: "center"
+      },
+      xaxis: {
+        categories: this.uniqueKeys
+      },
+      fill: {
+        opacity: 1
+      },
+      legend: {
+        position: "top",
+        horizontalAlign: "center",
+        offsetX: 0
+      }
+    };
+    return chartOptions;
+  }
+}
+
+class PieGraph {
+  constructor(data, filter, name) {
+    this.filteredData = data.filter(item => item.data[filter]);
+    this.name = name;
+    this.totalItems = this.filteredData.map(item => item.data[filter].value);
+    this.uniqueOptions = [...new Set(this.totalItems)];
+    this.uniqueOptionsSorted = this.sortOptions();
+    this.aggregatedData = this.uniqueOptionsSorted.map(option => {
+      let count = 0;
+      for (let i = 0; i < this.totalItems.length; i++) {
+        if (this.totalItems[i] === option) {
+          count++;
+        }
+      }
+      return count;
+    });
+    this.configObject = {
+      series: this.aggregatedData,
+      chartOptions: {
+        chart: {
+          type: "pie",
+          height: "300",
+          fontFamily:
+            "Roboto,-apple-system, system-ui, BlinkMacSystemFont, 'Segoe UI', Roboto,'Helvetica Neue', Arial, sans-serif"
+        },
+        labels: this.uniqueOptionsSorted,
+        title: {
+          text: this.name,
+          align: "center"
+        },
+        legend: {
+          position: "bottom",
+          horizontalAlign: "center"
+        }
+      }
+    };
+  }
+
+  //Methods
+  sortOptions() {
+    if (this.uniqueOptions.includes("Very Useful")) {
+      return ["Very Useful", "Somewhat Useful", "Not Useful", "N/A"];
+    }
+
+    if (this.uniqueOptions.includes("Strongly Agree")) {
+      return [
+        "Strongly Agree",
+        "Agree",
+        "Neutral",
+        "Disagree",
+        "Strongly Disagree"
+      ];
+    }
+
+    if (this.uniqueOptions.includes("Maybe")) {
+      return ["Yes", "Maybe", "No"];
+    }
+
+    if (this.uniqueOptions.includes("Increase and expand to other platforms")) {
+      return [
+        "Increase and expand to other platforms",
+        "Increase",
+        "Remain the same",
+        "Decrease",
+        "Desist"
+      ];
+    }
+
+    return this.uniqueOptions.sort();
+  }
+}
+
+class StandardPie {
+  constructor(data, filter, name) {
+    this.name = name;
+    this.filteredData = data.filter(item => item.data[filter]);
+    this.totalItems = this.filteredData.map(item => item.data[filter].value);
+    this.itemsWithoutOther = this.totalItems.map(item =>
+      item.replace("Other: ", "")
+    );
+    this.itemsStandardized = this.itemStandardizer(this.itemsWithoutOther);
+    this.itemUnique = [...new Set(this.itemsStandardized)];
+    this.counteditem = this.count(this.itemsStandardized);
+    this.aggregatedData = this.aggregateData(this.counteditem);
+
+    this.configObject = {
+      series: this.aggregatedData,
+      chartOptions: {
+        chart: {
+          type: "pie",
+          height: "300",
+          fontFamily:
+            "Roboto,-apple-system, system-ui, BlinkMacSystemFont, 'Segoe UI', Roboto,'Helvetica Neue', Arial, sans-serif"
+        },
+        labels: this.itemUnique,
+        title: {
+          text: this.name,
+          align: "center"
+        },
+        legend: {
+          position: "bottom",
+          horizontalAlign: "center"
+        }
+      }
+    };
+  }
+
+  itemStandardizer(input) {
+    const standardizedArray = input.map(
+      item =>
+        item
+          .replace(/M(an)?\b/i, "Male")
+          .replace(/F(emale)?\b/i, "Female")
+          .charAt(0)
+          .toUpperCase() + item.slice(1)
+    );
+    return standardizedArray;
+  }
+
+  count(inputArr) {
+    let countedItems = inputArr.reduce(function(allItems, item) {
+      if (item in allItems) {
+        allItems[item]++;
+      } else {
+        allItems[item] = 1;
+      }
+      return allItems;
+    }, {});
+    return countedItems;
+  }
+
+  aggregateData(input) {
+    const arr = Object.keys(input).map(item => input[item]);
+    return arr;
+  }
+}
