083abed87c6f5357e4bee6e9d0d3e2153dea93d4
Ryan Feigenbaum <rfeigenb@ND.EDU>
2020-04-27 10:44:32 -0400

    updated links on navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,18 +2,12 @@
   <b-navbar class="is-primary" :fixed-top="true">
     <template slot="brand">
       <b-navbar-item tag="router-link" :to="{ path: '/' }">
-        <g-image
-          class="image"
-          src="~/assets/logo.png"
-          width="500"
-          alt="HSS logo"
-        />
+        <g-image class="image" src="~/assets/logo.png" width="500" alt="HSS logo" />
       </b-navbar-item>
     </template>
 
     <template slot="start">
-      <b-navbar-item :active="true" href="#">Home</b-navbar-item>
-      <b-navbar-item href="#">About</b-navbar-item>
+      <b-navbar-item :active="true" href="/">Home</b-navbar-item>
       <b-navbar-dropdown label="Results">
         <b-navbar-item href="#general">General</b-navbar-item>
         <b-navbar-item href="#accessibility">Accessibility</b-navbar-item>
@@ -26,16 +20,20 @@
     <template slot="end">
       <b-navbar-item tag="div">
         <div class="buttons">
-          <a href="/" class="button is-danger is-inverted is-small">
-            <b-icon icon="heart" size="is-small" />
-            <span><strong>Donate</strong></span>
-          </a>
           <a
-            href="https://hssonline.formstack.com/forms/website_redesign"
-            class="button is-small"
+            href="https://interland3.donorperfect.net/weblink/WebLink.aspx?name=E343847&id=18"
+            class="button is-danger is-inverted is-small"
           >
+            <b-icon icon="heart" size="is-small" />
+            <span>
+              <strong>Donate</strong>
+            </span>
+          </a>
+          <a href="https://hssonline.formstack.com/forms/website_redesign" class="button is-small">
             <b-icon icon="clipboard-text" size="is-small" />
-            <span><strong>Take the Survey</strong></span>
+            <span>
+              <strong>Take the Survey</strong>
+            </span>
           </a>
         </div>
       </b-navbar-item>
